c1187fbad23a9517bdf387e970b5ab1b409fb66b
dungbaoviec123 <dungtran160820051508@gmail.com>
2025-04-07 15:06:33 +0700

    Add DTO\ViewModel

---
+++ CuaHangMayTinh/DTO/ViewModel/ProductDetailView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuaHangMayTinh.DTO.ViewModel
+{
+    public class ProductDetailView
+    {
+        public int Product_Id { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; } // Laptop / PC / Accessories
+        public string Specification { get; set; }
+        public string Colour { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
---
+++ CuaHangMayTinh/DTO/ViewModel/ReceiptDetailView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuaHangMayTinh.DTO.ViewModel
+{
+    public class ReceiptDetailView
+    {
+        public int Receipt_Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal Discount { get; set; }
+        public decimal FinalTotal { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        public string CustomerName { get; set; }
+        public string EmployeeName { get; set; }
+    }
+
+}
