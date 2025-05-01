591157fe0d9ac9ba0be44ff3463ecf54b8ee9ef2
hahuy1708 <hahuy17082005@gmail.com>
2025-05-01 15:09:14 +0700

    Update Receipt.cs

--- CuaHangMayTinh/DTO/Staff/Receipt.cs
+++ CuaHangMayTinh/DTO/Staff/Receipt.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CuaHangMayTinh.DTO.Common;
 
 namespace CuaHangMayTinh.DTO.Staff
 {
     public class Receipt
     {
         public int Receipt_Id { get; set; }
-        public int Details_Id { get; set; }
-        public decimal TotalAmount { get; set; }
         public DateTime ReceiptDate { get; set; }
         public int Customer_Id { get; set; }
         public int Employee_Id { get; set; }
+
+        // quan hệ 1–n: một hoá đơn có nhiều details
+        public List<Details> Details { get; set; } = new List<Details>();
+        public decimal? TotalAmount { get; set; }
     }
 }
