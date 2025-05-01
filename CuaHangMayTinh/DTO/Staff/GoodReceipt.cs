6f3b515bbe7554bdca42f044fa28e92e2db06218
hahuy1708 <hahuy17082005@gmail.com>
2025-05-01 15:18:23 +0700

    Update GoodReceipt.cs

--- CuaHangMayTinh/DTO/Staff/GoodReceipt.cs
+++ CuaHangMayTinh/DTO/Staff/GoodReceipt.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CuaHangMayTinh.DTO.Common;
 
 namespace CuaHangMayTinh.DTO.Staff
 {
     public class GoodsReceipt
     {
-        public int GoodsReceipt_Id { get; set; }
-        public int Employee_Id { get; set; }
-        public int Details_Id { get; set; }
-        public decimal TotalAmount { get; set; }
-        public DateTime GoodsReceiptDate { get; set; }
+        public int      GoodsReceipt_Id   { get; set; }
+        public DateTime GoodsReceiptDate  { get; set; }
+        public int      Employee_Id       { get; set; }
+
+        // Quan hệ 1–n: một GoodsReceipt có nhiều Details
+        public List<Details> Details { get; set; } = new List<Details>();
+
+        public decimal? TotalAmount { get; set; }
     }
 }
