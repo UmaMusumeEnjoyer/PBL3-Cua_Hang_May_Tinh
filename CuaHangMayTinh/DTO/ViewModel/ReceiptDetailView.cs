75e92644ea1eba82cd0a16dadbfe461a67873142
hahuy1708 <hahuy17082005@gmail.com>
2025-05-01 15:18:46 +0700

    Update ReceiptDetailView.cs

--- CuaHangMayTinh/DTO/ViewModel/ReceiptDetailView.cs
+++ CuaHangMayTinh/DTO/ViewModel/ReceiptDetailView.cs
@@ -1,23 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CuaHangMayTinh.DTO.ViewModel
 {
     public class ReceiptDetailView
     {
         public int Receipt_Id { get; set; }
-        public string ProductName { get; set; }
-        public int Quantity { get; set; }
-        public decimal ProductPrice { get; set; }
-        public decimal TotalAmount { get; set; }
-        public decimal Discount { get; set; }
-        public decimal FinalTotal { get; set; }
         public DateTime ReceiptDate { get; set; }
         public string CustomerName { get; set; }
         public string EmployeeName { get; set; }
+
+        // Danh sách các dòng hàng
+        public List<LineItem> Items { get; set; } = new List<LineItem>();
+
+        public decimal TotalAmount { get; set; }
+        public decimal Discount { get; set; }
+        public decimal FinalTotal { get; set; }
     }
 
+    public class LineItem
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal LineTotal => Quantity * ProductPrice;
+    }
 }
