7a56c8872daf31b1477018421ed2c5264424fcd2
hahuy1708 <hahuy17082005@gmail.com>
2025-05-01 15:02:24 +0700

    update Details.cs

--- CuaHangMayTinh/DTO/Common/Details.cs
+++ CuaHangMayTinh/DTO/Common/Details.cs
@@ -12,7 +12,9 @@ namespace CuaHangMayTinh.DTO.Common
         public int Product_Id { get; set; }
         public int Quantity { get; set; }
         public decimal ProductPrice { get; set; }
-      
-    }
 
-}  
+        // khóa ngoại, chỉ một trong hai có giá trị
+        public int? Receipt_Id { get; set; }
+        public int? GoodsReceipt_Id { get; set; }
+    }
+}
