426010095a809037fc43ca79f9b299cfe2a50914
hahuy1708 <hahuy17082005@gmail.com>
2025-05-02 20:21:01 +0700

    update

--- CuaHangMayTinh/DAL/DetailsDAO.cs
+++ CuaHangMayTinh/DAL/DetailsDAO.cs
@@ -1,6 +1,10 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using CuaHangMayTinh.DAL;
 namespace CuaHangMayTinh.DAL
 {
-    public class DetailsDAO
+    public class DetailsDAO : DbConnect
     {
         
     }
