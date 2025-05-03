6db016305bb9747f3358602586aba762cf7a1515
hahuy1708 <hahuy17082005@gmail.com>
2025-05-03 09:21:54 +0700

    update read method for Detail

--- CuaHangMayTinh/DAL/DetailsDAO.cs
+++ CuaHangMayTinh/DAL/DetailsDAO.cs
@@ -6,6 +6,28 @@ namespace CuaHangMayTinh.DAL
 {
     public class DetailsDAO : DbConnect
     {
-        
+        public DataTable GetAllDetails()
+        {
+            const string sql = @"SELECT d.*, p.productName, r.receiptDate, gr.goodsReceiptDate
+                                 FROM Details d
+                            LEFT JOIN Product p ON d.Product_Id = p.Product_Id
+                            LEFT JOIN Receipt r ON d.Receipt_Id = r.Receipt_Id
+                            LEFT JOIN Goods_Receipt gr ON d.GoodsReceipt_Id = gr.GoodsReceipt_Id";
+            return GetData(sql);
+        }
+        public DataTable GetDetailsByReceipt(int receiptId)
+        {
+            const string sql = @"SELECT d.*
+                                 FROM Details d
+                                WHERE d.Receipt_Id = @ReceiptId";
+            return GetData(sql, new[] { new SqlParameter("@ReceiptId", receiptId) });
+        }
+        public DataTable GetDetailsByGoodsReceipt(int goodsReceiptId)
+        {
+            const string sql = @"SELECT d.*
+                                 FROM Details d
+                                WHERE d.GoodsReceipt_Id = @GoodsReceiptId";
+            return GetData(sql, new[] { new SqlParameter("@GoodsReceiptId", goodsReceiptId) });
+        }
     }
 }
