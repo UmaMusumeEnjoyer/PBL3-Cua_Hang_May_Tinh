dc0e092e33278cb0804fdd584f439dee2d81399a
hahuy1708 <hahuy17082005@gmail.com>
2025-05-06 20:15:30 +0700

    add ChartBUS

---
+++ CuaHangMayTinh/BLL/ChartBUS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CuaHangMayTinh.DAL;
+using CuaHangMayTinh.DTO.Common;
+
+namespace CuaHangMayTinh.BLL
+{
+    public class ChartBUS
+    {
+        private readonly ChartDAO _chartDAO = new ChartDAO();
+        public ChartBUS()
+        {
+            _chartDAO = new ChartDAO();
+        }
+        public List<ChartData> GetMonthlyRevenue(int year)
+        {
+            return _chartDAO.GetMonthlyRevenue(year);
+        }
+    }
+}
