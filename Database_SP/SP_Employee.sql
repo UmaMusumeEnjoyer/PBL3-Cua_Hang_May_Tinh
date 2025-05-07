94ed1bb448c28b5aedc0ae47d9c6dd88d886b346
hahuy1708 <146620248+hahuy1708@users.noreply.github.com>
2025-05-07 09:37:40 +0700

    Update SP_Employee.sql

--- Database_SP/SP_Employee.sql
+++ Database_SP/SP_Employee.sql
@@ -1,1 +1,20 @@
+CREATE PROCEDURE sp_Employee_GetAll
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT * FROM Employee;
+END
+GO
+
+-- Lấy nhân viên theo ID
+CREATE PROCEDURE sp_Employee_GetById
+    @Employee_Id INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT * 
+    FROM Employee 
+    WHERE Employee_Id = @Employee_Id;
+END
+GO
 
