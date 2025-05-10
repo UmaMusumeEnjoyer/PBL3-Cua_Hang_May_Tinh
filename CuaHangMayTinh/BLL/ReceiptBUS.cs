38da85e11231a34ceded38b690c6225764000a24
hahuy1708 <hahuy17082005@gmail.com>
2025-05-10 23:02:55 +0700

    update ReceiptBUS.cs

--- CuaHangMayTinh/BLL/ReceiptBUS.cs
+++ CuaHangMayTinh/BLL/ReceiptBUS.cs
@@ -1,7 +1,14 @@
+using System.Data;
+using CuaHangMayTinh.DAL;
+using CuaHangMayTinh.DTO.Staff;
 namespace CuaHangMayTinh.BLL
 {
     public class ReceiptBUS
     {
+        private readonly ReceiptDAO _receiptDAO = new ReceiptDAO();
+        public DataTable GetAllReceipts() => _receiptDAO.GetAllReceipts();
+        public Receipt GetReceipt(int id)   => _receiptDAO.GetFullReceipt(id);
+        public int CreateReceipt(Receipt r)  => _receiptDAO.CreateReceipt(r);
         
     }
 }
